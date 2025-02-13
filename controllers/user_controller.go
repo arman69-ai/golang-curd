844ae16416022266da4833eae928b2d984ec7350
arman69-ai <arman6912jkt@gmail.com>
2025-02-13 10:28:21 +0700

    Create user_controller.go

---
+++ controllers/user_controller.go
@@ -0,0 +1,67 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/arman69-ai/golang-crud/database"
+	"github.com/arman69-ai/golang-crud/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Create User (POST)
+func CreateUser(c *gin.Context) {
+	var user models.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	database.DB.Create(&user)
+	c.JSON(http.StatusOK, user)
+}
+
+// Get All Users (GET)
+func GetUsers(c *gin.Context) {
+	var users []models.User
+	database.DB.Find(&users)
+	c.JSON(http.StatusOK, users)
+}
+
+// Get User by ID (GET)
+func GetUser(c *gin.Context) {
+	var user models.User
+	id := c.Param("id")
+	if err := database.DB.First(&user, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
+
+// Update User (PUT)
+func UpdateUser(c *gin.Context) {
+	var user models.User
+	id := c.Param("id")
+	if err := database.DB.First(&user, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	database.DB.Save(&user)
+	c.JSON(http.StatusOK, user)
+}
+
+// Delete User (DELETE)
+func DeleteUser(c *gin.Context) {
+	var user models.User
+	id := c.Param("id")
+	if err := database.DB.First(&user, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+	database.DB.Delete(&user)
+	c.JSON(http.StatusOK, gin.H{"message": "User deleted"})
+}
