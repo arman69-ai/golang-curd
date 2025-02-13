e547a07526657847a042df2874f6d131744fdd03
arman69-ai <arman6912jkt@gmail.com>
2025-02-13 10:34:59 +0700

    Create user_routes.go

---
+++ routes/user_routes.go
@@ -0,0 +1,20 @@
+package routes
+
+import (
+	"github.com/arman69-ai/golang-crud/controllers"
+	"github.com/arman69-ai/golang-crud/middlewares"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(router *gin.Engine) {
+	// Lindungi semua route user
+	protected := router.Group("/users")
+	protected.Use(middlewares.AuthMiddleware())
+	{
+		protected.GET("", controllers.GetUsers)
+		protected.GET("/:id", controllers.GetUser)
+		protected.PUT("/:id", controllers.UpdateUser)
+		protected.DELETE("/:id", controllers.DeleteUser)
+	}
+}
